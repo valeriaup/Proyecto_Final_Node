a6bb1f681c1773a4a8366bbbcc6d9f4dfec7ee10
Valeria Usuga <usugapenagosvaleria@gmail.com>
2026-03-20 13:40:20 -0500

    feat(db.js) Base de datos

--- db.js
+++ db.js
@@ -7,3 +7,45 @@ const db = new sqlite3.Database('./database.db', (err) => {
     console.log('Base de datos conectada');
   }
 });
+
+db.run(`PRAGMA foreign_key = ON`)
+
+db.run(`CREATE TABLE IF NOT EXISTS Estudiantes(
+  estudianteId INTEGER PRIMARY KEY AUTOINCREMENT,
+  nombre       TEXT    NOT NULL,
+  apellido     TEXT    NOT NULL,
+  genero       TEXT    NOT NULL,
+  email        TEXT    NOT NULL UNIQUE
+  )`);
+
+db.run(`CREATE TABLE IF NOT EXISTS Profesores(
+  profesorId INTEGER PRIMARY KEY AUTOINCREMENT,
+  nombre       TEXT    NOT NULL,
+  apellido     TEXT    NOT NULL,
+  especialidad TEXT    NOT NULL,
+  email        TEXT    NOT NULL UNIQUE
+  )`);
+
+db.run(`CREATE TABLE IF NOT EXISTS Cursos(
+  cursoId     INTEGER PRIMARY KEY AUTOINCREMENT,
+  nombre      TEXT    NOT NULL,
+  profesorId  INTEGER NOT NULL,
+  FOREIGN KEY (profesorId) REFERENCES Profesores(profesorId)
+  )`)
+
+db.run(`CREATE TABLE IF NOT EXISTS Inscripciones(
+  inscripcionId INTEGER PRIMARY KEY AUTOINCREMENT,
+  estudianteId  INTEGER NOT NULL,
+  cursoId       INTEGER NOT NULL,
+  FOREIGN KEY   (estudianteId) REFERENCES Estudiantes(estudianteId),
+  FOREIGN KEY   (cursoId)      REFERENCES Cursos(cursoId)
+  )`)
+
+db.run(`CREATE TABLE IF NOT EXISTS Notas(
+  notaId        INTEGER PRIMARY KEY AUTOINCREMENT,
+  inscripcionId INTEGER NOT NULL,
+  valor         REAL NOT NULL,
+  FOREIGN KEY   (inscripcionId) REFERENCES Inscripciones(inscripcionId)
+  )`)
+  
+  module.exports = db;
